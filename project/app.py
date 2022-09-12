0664244a4ce24719a1e3f70647533d7b168a19dd
lishubin <2697601945@qq.com>
2022-09-12 17:18:58 +0800

    调用escape

--- project/app.py
+++ project/app.py
@@ -1,19 +1,12 @@
-from flask import Flask,render_template
-from dist.vsearch import search4letters
+from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello()->str:  # put application's code here
-    return 'Hello World from Flask'
+def hello_world():  # put application's code here
+    return 'Hello World!'
 
-@app.route('/search4')
-def do_search()-> str:
-    return str(search4letters('life,the universe,and everything','aeiou,!'))
-
-@app.route('/entry')
-def entry_page()-> 'html':
-    return render_template('entry.html',the_title = 'Welcome to search4letters on the web!')
 
 if __name__ == '__main__':
     app.run()
--- project/week4_head_fitst_chapter4-5/vsearch4web.py
+++ project/week4_head_fitst_chapter4-5/vsearch4web.py
@@ -2,31 +2,41 @@
 # @Author: 李树斌
 # @File : vsearch4web.py
 # @Software :PyCharm
-# import vsearch
-from flask import Flask,render_template,request
-from dist.vsearch import search4letters
+
+from flask import Flask,render_template,request,escape
+
+
+from vsearch import search4letters
+
 app = Flask(__name__)
+def log_request(req:'flask_request',res:str) ->None:
+    with open('vsearch.log','a') as log:
+        print(req,res,file=log)
 
-
-# @app.route('/')
-# def hello()-> 302:  # put application's code here
-#     return redirect('/entry')
-
-@app.route('/search4',methods = ['POST'])
+@app.route('/search4',methods = ['POST','GET'])
 def do_search()-> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are the results'
     results = str(search4letters(phrase,letters))
+    log_request(request,results)
     return render_template('results.html',the_phrase = phrase,
                                         the_letters = letters,
                                         the_tilte = title,
                                         the_results = results)
+
+@app.route('/viewlog')
+def view_the_log()->str:
+    with open('vsearch.log') as log:
+        contents = log.read()
+    return escape(contents)
+
+
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html',the_title = 'Welcome to search4letters on the web!')
 
 
-# if __name__ == '__main__':
-app.run(debug=True, host="127.0.0.1", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, host="127.0.0.1", port=5000)
