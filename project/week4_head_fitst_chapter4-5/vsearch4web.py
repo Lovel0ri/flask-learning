963c6a6a8c89a62aa4d58ae6c776c097df4bab48
lishubin <2697601945@qq.com>
2022-09-12 17:42:49 +0800

    记录特定的web请求属性

--- project/week4_head_fitst_chapter4-5/vsearch4web.py
+++ project/week4_head_fitst_chapter4-5/vsearch4web.py
@@ -5,19 +5,24 @@
 
 from flask import Flask,render_template,request,escape
 
-
 from vsearch import search4letters
 
 app = Flask(__name__)
 def log_request(req:'flask_request',res:str) ->None:
     with open('vsearch.log','a') as log:
-        print(req,res,file=log)
+        """从web应用的html表单提交的数据"""
+        print(req.form,file=log)
+        """运行web浏览器的ip地址"""
+        print(req.remote_addr,file=log)
+        """提交数据的浏览器的标识"""
+        print(req.user_agent,file=log)
+        print(res,file=log)
 
 @app.route('/search4',methods = ['POST','GET'])
 def do_search()-> 'html':
+    title = 'Here are the results'
     phrase = request.form['phrase']
     letters = request.form['letters']
-    title = 'Here are the results'
     results = str(search4letters(phrase,letters))
     log_request(request,results)
     return render_template('results.html',the_phrase = phrase,
