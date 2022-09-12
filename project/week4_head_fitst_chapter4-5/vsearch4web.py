b2ec3e37f97975ec82f33c9be2c2ad1ed9b4b305
lishubin <2697601945@qq.com>
2022-09-13 00:41:11 +0800

    修复type error报错

--- project/week4_head_fitst_chapter4-5/vsearch4web.py
+++ project/week4_head_fitst_chapter4-5/vsearch4web.py
@@ -36,8 +36,6 @@ def view_the_log()->'html':
             contents.append([])
             for item in line.split('|'):
                 contents[-1].append(escape(item))
-    return  str(contents)
-
     titles = ['Form Data','Remote_addr','User_agent','Results']
     return render_template('viewlog.html',
                            the_title='View Log',
