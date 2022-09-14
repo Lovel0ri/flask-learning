d0fca04a3a1fe78a6808cb468db1524f5b195b2c
lishubin <2697601945@qq.com>
2022-09-14 16:11:48 +0800

    最终版

--- project/week4_head_fitst_chapter4-5/vsearch4web.py
+++ project/week4_head_fitst_chapter4-5/vsearch4web.py
@@ -4,17 +4,33 @@
 # @Software :PyCharm
 
 from flask import Flask,render_template,request,escape
-from DBcm import  UseDatabase
+from DBcm import UseDatabase
 from vsearch import search4letters
 
 app = Flask(__name__)
+
 app.config['dbconfig'] = {'host':'127.0.0.1',
-                'user':'vsearch',
-                'password':'vsearchpasswd',
+                'user':'root',
+                'password':'aA2697601945',
                 'database':'vsearchlogDB',}
+
 """写入记录日志文件"""
 def log_request(req:'flask_request',res:str) ->None:
     with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """insert into log
+                        (phrase,letters,ip,browser_string,results)
+                        values
+                        (%s,%s,%s,%s,%s)"""
+
+        cursor.execute(_SQL, (req.form['phrase'],
+                              req.form['letters'],
+                              req.remote_addr,
+                              req.user_agent.browser,
+                              res,))
+
+        conn.commit()
+        cursor.close()
+        conn.close()
     # 定义连接属性
     # dbconfig = {'host':'127.0.0.1',
     #             'user':'vsearch',
@@ -28,21 +44,7 @@ def log_request(req:'flask_request',res:str) ->None:
     # # #创建游标
     # cursor = conn.cursor()
     # #创建字符串，包含想要使用的查询
-    _SQL = """insert into log1
-                (phrase,letters,ip,browser_string,results)
-                values
-                (%s,%s,%s,%s,%s)"""
 
-
-    cursor.execute(_SQL,(req.form['phrase'],
-                         req.form['letters'],
-                         req.remote_addr,
-                         req.user_agent.browser,
-                         res,))
-
-    conn.commit()
-    cursor.close()
-    conn.close()
     # with open('vsearch.log','a') as log:
     #     """从web应用的html表单提交的数据，运行web浏览器的ip地址，提交数据的浏览器的标识"""
     #     print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
@@ -60,17 +62,23 @@ def do_search()-> 'html':
 
 @app.route('/viewlog')
 def view_the_log()->'html':
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """select phrase,letters,ip,browser_string,results
+                from log"""
+        #将查询发送到数据库然后获取结果。将获取的数据赋到“contents"
+        cursor.excute(_SQL)
+        contents = cursor.fetchall()
 
 
-   """ contents = []
-    with open('vsearch.log') as log:
-        for line in log:
-            contents.append([])
-            for item in line.split('|'):
-                contents[-1].append(escape(item))"""
+#   """ contents = []
+#    with open('vsearch.log') as log:
+ #       for line in log:
+  #          contents.append([])
+   #         for item in line.split('|'):
+    #            contents[-1].append(escape(item))"""
 
 
-    titles = ['Form Data','Remote_addr','User_agent','Results']
+    titles = ['Phrase','Letters','Remote_addr','User_agent','Results']
     return render_template('viewlog.html',
                            the_title='View Log',
                            the_row_titles=titles,
@@ -85,4 +93,4 @@ def entry_page() -> 'html':
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=True, host="127.0.0.1")
